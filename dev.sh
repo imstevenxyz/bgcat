e66852518b7f714e8e8181f947a54cab9dd20904
Steven I. <commits@imsteven.xyz>
2023-10-15 15:30:10 +0200

    Update SurrealDB to v1.0.0

--- dev.sh
+++ dev.sh
@@ -3,7 +3,7 @@ export BGCAT_LOG_LEVEL="debug";
 export BGCAT_DB_START_LOCAL="false";
 
 
-surreal start --user root --pass root --bind 127.0.0.1:8001 file:./data/bgcat.db &
+surreal start --auth --user root --pass root --bind 127.0.0.1:8001 file:./data/bgcat.db &
 cargo watch --quiet \
     --ignore container \
     --ignore docs \
